8abb2b22828187d81bd9e1edee365969299515c1
Aleksandr Eremin <aleksandr.i.eremin@yandex.ru>
2023-07-27 16:14:48 +0300

    Fix/m2 1288 remove password check when save and publish applet (#710)
    * show password popup only when new applet
    
    * moved encryption generation after async actions
    
    * moved encryption keys into session storage
    
    * cleared session storage on logout
    
    * fixed paths

---
+++ src/shared/hooks/useEncryptionStorage.ts
@@ -0,0 +1,37 @@
+import { useCheckIfNewApplet } from 'shared/hooks/useCheckIfNewApplet';
+import { auth } from 'modules/Auth';
+
+const getKey = (ownerId: string, appletId: string) => `pwd/${ownerId}/${appletId}`;
+
+export const useEncryptionStorage = () => {
+  const isNewApplet = useCheckIfNewApplet();
+  const userData = auth.useData();
+  const ownerId = String(userData?.user?.id) || '';
+
+  const getAppletPrivateKey = (appletId: string) => {
+    if (isNewApplet || !appletId || !ownerId) return '';
+
+    const key = getKey(ownerId, appletId);
+    try {
+      return JSON.parse(sessionStorage.getItem(key) as string);
+    } catch {
+      sessionStorage.removeItem(key);
+
+      return '';
+    }
+  };
+
+  const setAppletPrivateKey = (appletId: string, privateKey: number[]) => {
+    if (!appletId || !ownerId) return;
+
+    const key = getKey(ownerId, appletId);
+    if (!privateKey) return sessionStorage.removeItem(key);
+
+    sessionStorage.setItem(key, JSON.stringify(privateKey));
+  };
+
+  return {
+    getAppletPrivateKey,
+    setAppletPrivateKey,
+  };
+};
